d6c7861c5efd3a96d428a476bee4c6960c254ff2
sammy wlodawski <samwlodawski@gmail.com>
2024-12-09 15:44:40 -0500

    Setting up primary layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64'>
+      {/*NAVBAR*/}
+      {/*BREADCRUMB*/}
+      {/*INTRODUCTION*/}
+      {/*FEATURED POSTS*/}
+      {/*POST LIST*/}
+      {/*NAVBAR*/}
+      {/*NAVBAR*/}
+    </div>
   )
 }
 
