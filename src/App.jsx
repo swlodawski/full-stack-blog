e48b9a7d0ed5e412a36e8f5d3124d3cfbee903f9
sammy wlodawski <samwlodawski@gmail.com>
2024-12-09 15:56:29 -0500

    Setting up nav bar logo.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ const App = () => {
   return (
     <div className='px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64'>
       {/*NAVBAR*/}
+      <Navbar />
       {/*BREADCRUMB*/}
       {/*INTRODUCTION*/}
       {/*FEATURED POSTS*/}
       {/*POST LIST*/}
-      {/*NAVBAR*/}
-      {/*NAVBAR*/}
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,16 @@
 const Navbar = () => {
     return(
-        <div className="">Navbar</div>
+        <div className="">
+        {/*LOGO*/}
+        <div className="">
+            <img src="/logo.png" className="w-8 h-8" alt="" />
+            <span>lamalogo</span>
+        </div>
+        {/*BREADCRUMB*/}
+        {/*INTRODUCTION*/}
+        {/*FEATURED POSTS*/}
+        {/*POST LIST*/}
+        </div>
     )
 }
 
