fff6e0121ff91f37d4d1e12166c6f90e90cb659c
sammy wlodawski <samwlodawski@gmail.com>
2024-12-09 16:47:25 -0500

    Styling and adjusting logo and nav bar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react"
+
 const Navbar = () => {
-    return(
-        <div className="">
+    const [open, setOpen ] = useState(false)
+    return (
+        <div className="w-full h-16 md:h-20 flex items-center justify-between">
         {/*LOGO*/}
-        <div className="">
+        <div className='flex items-center gap-4 text-2xl font-bold'>
             <img src="/logo.png" className="w-8 h-8" alt="" />
-            <span>lamalogo</span>
+            <span>lamalog</span>
         </div>
-        {/*BREADCRUMB*/}
-        {/*INTRODUCTION*/}
+        {/*MOBILE MENY*/}
+        <div className="md:hidden">
+            <div className="cursor-pointervtext-4xl"
+            onAuxClick={() => setOpen((prev) => !prev)}>
+                {open ? "X" : "|"}
+            </div>
+        </div>
+        {/*DESKTOP MENU*/}
+        <div className="hidden md:flex">D</div>
         {/*FEATURED POSTS*/}
         {/*POST LIST*/}
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
