038a9aed60bb7baf6cc2c8955cc4d16cd05ac7ee
sammy wlodawski <samwlodawski@gmail.com>
2024-12-09 21:52:29 -0500

    Changes to formatting.

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { 
   createBrowserRouter,
   RouterProvider,
-  Route, 
-  Link,
 } from "react-router-dom";
 
 const router = createBrowserRouter([
 {  
   path:"/",
-  element: <Homepage />,
+  element: <Homepage/>,
 },
 {
-  path: "/posts",
-  element: <PostListPage />,
+  path: "/:slug",
+  element: <PostListPage/>,
 },
 {
   path: "/write",
-  element: <Write />,
+  element: <Write/>,
 },
 {
   path: "/login",
-  element: <LoginPage />,
+  element: <LoginPage/>,
 },
 {
-  path: "/register",
-  element: <RegisterPage />,
+  path: "/:slug",
+  element: <RegisterPage/>,
 },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <App />
   </StrictMode>,
 );
